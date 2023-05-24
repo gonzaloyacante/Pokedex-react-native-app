95b567341caa19af1f47d6ab8fc288a750f25443
Gonzalo Yacante <gyacante9@gmail.com>
2023-05-24 10:34:04 +0100

    Creando array de pokemons favoritos

---
+++ src/components/Pokemon/Favorite.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { addPokemonFavoriteApi } from '../../api/favorite'
+
+const Favorite = (props) => {
+  const { id } = props;
+
+  const addFavorite = async () => {
+    await addPokemonFavoriteApi(id);
+  };
+
+  return (
+    <Icon
+      name="heart"
+      color="#fff"
+      size={20}
+      onPress={addFavorite}
+      style={{ marginRight: 20 }}
+    />
+  );
+};
+
+export default Favorite;
--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,15 +1,31 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Text, Button } from "react-native";
+// import React, { useState, useEffect } from "react";
+import { getPokemonsFavoriteApi } from "../api/favorite";
 
 const Favorite = () => {
+  // const [favorites, setFavorites] = useState(null);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await getPokemonsFavoriteApi();
+  //     console.log(response);
+  //   })();
+  // }, []);
+
+  const checkFavorites = async () => {
+    const response = await getPokemonsFavoriteApi();
+    console.log(response);
+  };
+
   return (
     <SafeAreaView>
       <Text>Favorite</Text>
+      <Button title="Obtener favoritos" onPress={checkFavorites} />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Favorite
+export default Favorite;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
